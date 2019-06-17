2e9435fcd453f1c4ab6441ddddf207b2f31f5c1d
quick-sort <quicksort@outlook.com>
2019-06-17 15:26:19 +0800

    fix access rights, menu, create customer

--- addons/project_hicto/models/models.py
+++ addons/project_hicto/models/models.py
@@ -5,17 +5,17 @@ from datetime import date
 
 class Activity(models.Model):
     _name = 'project_hicto.activity'
-    name = fields.Char(string='服务概述')
-    project_id = fields.Many2one('project_hicto.project', string='项目')
+    name = fields.Char(string='服务概述', required=True)
+    project_id = fields.Many2one('project_hicto.project', string='项目', required=True)
     company_id = fields.Many2one('res.partner', string='公司', related='project_id.company_id', readonly=True)
-    service_item = fields.Many2one('product.product', string='服务项', domain="[('type', '=', 'service')]")
+    service_item = fields.Many2one('product.product', string='服务项', domain="[('type', '=', 'service')]", required=True)
     service_way = fields.Selection([
         ('onsite', '现场'),
         ('remote', '远程'),
         ('prepare', '线下准备')
-        ], string="服务形式" )
+        ], string="服务形式" , default='remote')
     activity_start = fields.Datetime(string='开始时间')
-    expert_id = fields.Many2one('res.users', string='HiCTO组织者')
+    expert_id = fields.Many2one('res.users', string='组织者')
     participants = fields.Many2many('res.partner', string='客户参与者', domain="[('is_company', '=', False),('parent_id', '=', company_id)]")
     desc = fields.Html(string='服务详细描述')
     timesheet_ids = fields.One2many('project_hicto.timesheet', 'activity_id', string='工时')
@@ -24,14 +24,14 @@ class Timesheet(models.Model):
     _name = 'project_hicto.timesheet'
     _rec_name = 'expert_id'
     activity_id = fields.Many2one('project_hicto.activity', string='服务记录')
-    expert_id = fields.Many2one('res.users', string='HiCTO参与者')
+    expert_id = fields.Many2one('res.users', string='参与者')
     work_hour = fields.Float(string='小时', default=1.0)
     desc = fields.Html(string='工作内容')
 
 class Project(models.Model):
     _name = 'project_hicto.project'
-    name = fields.Char(string='项目名称')
-    company_id = fields.Many2one('res.partner', string='公司', domain="[('is_company', '=', True), ('id', '!=', 1)]")
+    name = fields.Char(string='项目名称', required=True)
+    company_id = fields.Many2one('res.partner', string='公司', domain="[('is_company', '=', True), ('id', '!=', 1)]", required=True)
     contact_id = fields.Many2one('res.partner', string='客户联系人', domain="[('is_company','=',False), ('parent_id', '=', company_id)]")
     manager_id = fields.Many2one('res.users', string='项目负责人')
     expert_ids = fields.Many2many('res.users', string='项目参与者')
@@ -48,4 +48,5 @@ class Project(models.Model):
     def _get_status(self):
         today = fields.Date.today()
         for record in self:
-            record.status = 'open' if today < fields.Date.add(record.date_start,months=record.months) else 'close'
+            if record.date_start and record.months:
+                record.status = 'open' if today < fields.Date.add(record.date_start,months=record.months) else 'close'
